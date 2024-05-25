d77b10c18d876b07e92b78a2daa0cf1df25133a0
Julian Lobbes <julian@lobbes.dev>
2024-05-25 12:24:11 +0100

    SQUASHME: start adding signals collection

--- hyprpy/components/instances.py
+++ hyprpy/components/instances.py
@@ -14,10 +14,270 @@ from hyprpy.components.workspaces import Workspace
 from hyprpy.components.monitors import Monitor
 from hyprpy.utils import assertions, shell
 from hyprpy.utils.sockets import CommandSocket, EventSocket
-from hyprpy.utils.signals import Signal
+from hyprpy.utils.signals import DeprecatedSignal, Signal
 
 
 log = logging.getLogger(__name__)
+
+
+class SignalCollection:
+    """A collection of signals belonging to an :class:`~Instance`."""
+
+    def __init__(self):
+        #: Emits the following Signal Data when a keyboard's layout is changed:
+        #:
+        #:    .. table::
+        #:       :widths: auto
+        #:       :align: left
+        #:
+        #:       ================= ============ ===============================
+        #:       Name              Type         Description
+        #:       ================= ============ ===============================
+        #:       ``keyboard_name`` :class:`str` Name of the keyboard
+        #:       ``layout_name``   :class:`str` Name of the newly active layout
+        #:       ================= ============ ===============================
+        self.activelayout: Signal = Signal(self)
+
+        #: Emits the following Signal Data when the special workspace on a monitor changes:
+        #:
+        #:    .. table::
+        #:       :widths: auto
+        #:       :align: left
+        #:
+        #:       ================== ============ =====================================================================
+        #:       Name               Type         Description
+        #:       ================== ============ =====================================================================
+        #:       ``workspace_name`` :class:`str` :attr:`~hyprpy.components.workspaces.Workspace.name` of the workspace
+        #:       ``monitor_name``   :class:`str` :attr:`~hyprpy.components.monitors.Monitor.name` of the monitor
+        #:       ================== ============ =====================================================================
+        #:
+        #: When the special workspace is closed, ``workspace_name`` is an empty string.
+        self.activespecial: Signal = Signal(self)
+
+        #: Emits the following Signal Data when the active window is changed:
+        #:
+        #:    .. table::
+        #:       :widths: auto
+        #:       :align: left
+        #:
+        #:       ================ ============ =================================================================================
+        #:       Name             Type         Description
+        #:       ================ ============ =================================================================================
+        #:       ``window_class`` :class:`str` The newly active window's :attr:`~hyprpy.components.windows.Window.wm_class`
+        #:       ``window_title`` :class:`str` Name of the newly active window's :attr:`~hyprpy.components.windows.Window.title`
+        #:       ================ ============ =================================================================================
+        self.activewindow: Signal
+
+        #: Emits the following Signal Data when the active window is changed:
+        #:
+        #:    .. table::
+        #:       :widths: auto
+        #:       :align: left
+        #:
+        #:       ================== ============ ===========================================================================
+        #:       Name               Type         Description
+        #:       ================== ============ ===========================================================================
+        #:       ``window_address`` :class:`str` The newly active Window's :attr:`~hyprpy.components.windows.Window.address`
+        #:       ================== ============ ===========================================================================
+        self.activewindowv2: Signal = Signal(self)
+
+        #: Emits the following Signal Data when a window's floating state changes:
+        #:
+        #:    .. table::
+        #:       :widths: auto
+        #:       :align: left
+        #:
+        #:       ================== ============= ===============================================================
+        #:       Name               Type          Description
+        #:       ================== ============= ===============================================================
+        #:       ``window_address`` :class:`str`  :attr:`~hyprpy.components.windows.Window.address` of the window
+        #:       ``is_floating``    :class:`bool` ``True`` if the window is floating, and ``False`` otherwise.
+        #:       ================== ============= ===============================================================
+        self.changefloatingmode: Signal = Signal(self)
+
+        #: Emits the following Signal Data when a layerSurface is unmapped:
+        #:
+        #:    .. table::
+        #:       :widths: auto
+        #:       :align: left
+        #:
+        #:       ============= ============= ===========
+        #:       Name          Type          Description
+        #:       ============= ============= ===========
+        #:       ``namespace`` :class:`str`  `Unknown`
+        #:       ============= ============= ===========
+        self.closelayer: Signal = Signal(self)
+
+        #: Emits the following Signal Data when a window is closed:
+        #:
+        #:    .. table::
+        #:       :widths: auto
+        #:       :align: left
+        #:
+        #:       ================== ============= ======================================================================
+        #:       Name               Type          Description
+        #:       ================== ============= ======================================================================
+        #:       ``window_address`` :class:`str`  :attr:`~hyprpy.components.windows.Window.address` of the closed window
+        #:       ================== ============= ======================================================================
+        self.closewindow: Signal = Signal(self)
+
+        #: Emitted upon a completed config reload.
+        #:
+        #: Does not emit any Signal Data.
+        self.configreloaded: Signal = Signal(self)
+
+        #: Emits the following Signal Data when a workspace is created:
+        #:
+        #:    .. table::
+        #:       :widths: auto
+        #:       :align: left
+        #:
+        #:       ================== ============= =============================
+        #:       Name               Type          Description
+        #:       ================== ============= =============================
+        #:       ``workspace_name`` :class:`str`  :attr:`~hyprpy.components.workspaces.Workspace.name` of the created workspace
+        #:       ================== ============= =============================
+        self.createworkspace: Signal = Signal(self)
+
+        #: Emits the following Signal Data when a workspace is created:
+        #:
+        #:    .. table::
+        #:       :widths: auto
+        #:       :align: left
+        #:
+        #:       ================== ============= =============================================================================
+        #:       Name               Type          Description
+        #:       ================== ============= =============================================================================
+        #:       ``workspace_id``   :class:`str`  :attr:`~hyprpy.components.workspaces.Workspace.id` of the created workspace
+        #:       ``workspace_name`` :class:`str`  :attr:`~hyprpy.components.workspaces.Workspace.name` of the created workspace
+        #:       ================== ============= =============================================================================
+        self.createworkspacev2: Signal = Signal(self)
+
+        #: Emits the following Signal Data when a workspace is destroyed:
+        #:
+        #:    .. table::
+        #:       :widths: auto
+        #:       :align: left
+        #:
+        #:       ================== ============= ===============================================================================
+        #:       Name               Type          Description
+        #:       ================== ============= ===============================================================================
+        #:       ``workspace_name`` :class:`str`  :attr:`~hyprpy.components.workspaces.Workspace.name` of the destroyed workspace
+        #:       ================== ============= ===============================================================================
+        self.destroyworkspace: Signal = Signal(self)
+
+        #: Emits the following Signal Data when a workspace is destroyed:
+        #:
+        #:    .. table::
+        #:       :widths: auto
+        #:       :align: left
+        #:
+        #:       ================== ============= ===============================================================================
+        #:       Name               Type          Description
+        #:       ================== ============= ===============================================================================
+        #:       ``workspace_id``   :class:`str`  :attr:`~hyprpy.components.workspaces.Workspace.id` of the destroyed workspace
+        #:       ``workspace_name`` :class:`str`  :attr:`~hyprpy.components.workspaces.Workspace.name` of the destroyed workspace
+        #:       ================== ============= ===============================================================================
+        self.destroyworkspacev2: Signal = Signal(self)
+
+        #: Emits the following Signal Data when the active monitor changes:
+        #:
+        #:    .. table::
+        #:       :widths: auto
+        #:       :align: left
+        #:
+        #:       ================== ============= ==================================================================================
+        #:       Name               Type          Description
+        #:       ================== ============= ==================================================================================
+        #:       ``monitor_name``   :class:`str`  :attr:`~hyprpy.components.monitors.Monitor.name` of the newly active monitor
+        #:       ``workspace_name`` :class:`str`  :attr:`~hyprpy.components.workspaces.Workspace.name` of the newly actove workspace
+        #:       ================== ============= ==================================================================================
+        self.focusedmon: Signal
+
+        #: Emits the following Signal Data when the fullscreen state of any window changes:
+        #:
+        #:    .. table::
+        #:       :widths: auto
+        #:       :align: left
+        #:
+        #:       ================= ============= ============================================================================================
+        #:       Name              Type          Description
+        #:       ================= ============= ============================================================================================
+        #:       ``is_fullscreen`` :class:`bool` ``True`` if fullscreen mode was activated, and ``False`` if fullscreen mode was deactivated.
+        #:       ================= ============= ============================================================================================
+        self.fullscreen: Signal = Signal(self)
+
+        #: emitted when ignoregrouplock is toggled.	0/1
+        self.ignoregrouplock: Signal = Signal(self)
+
+        #: emitted when lockgroups is toggled.	0/1
+        self.lockgroups: Signal = Signal(self)
+
+        #: emitted when a window requests a change to its minimized state. MINIMIZED is either 0 or 1.	WINDOWADDRESS,MINIMIZED
+        self.minimize: Signal = Signal(self)
+
+        #: emitted when a monitor is added (connected)	MONITORNAME
+        self.monitoradded: Signal = Signal(self)
+
+        #: emitted when a monitor is added (connected)	MONITORID,MONITORNAME,MONITORDESCRIPTION
+        self.monitoraddedv2: Signal = Signal(self)
+
+        #: emitted when a monitor is removed (disconnected)	MONITORNAME
+        self.monitorremoved: Signal = Signal(self)
+
+        #: emitted when the window is merged into a group. returns the address of a merged window	WINDOWADDRESS
+        self.moveintogroup: Signal = Signal(self)
+
+        #: emitted when the window is removed from a group. returns the address of a removed window	WINDOWADDRESS
+        self.moveoutofgroup: Signal = Signal(self)
+
+        #: emitted when a window is moved to a workspace	WINDOWADDRESS,WORKSPACENAME
+        self.movewindow: Signal = Signal(self)
+
+        #: emitted when a window is moved to a workspace	WINDOWADDRESS,WORKSPACEID,WORKSPACENAME
+        self.movewindowv2: Signal = Signal(self)
+        
+        #: emitted when a workspace is moved to a different monitor	WORKSPACENAME,MONNAME
+        self.moveworkspace: Signal = Signal(self)
+
+        #: emitted when a workspace is moved to a different monitor	WORKSPACEID,WORKSPACENAME,MONNAME
+        self.moveworkspacev2: Signal = Signal(self)
+
+        #: emitted when a layerSurface is mapped	NAMESPACE
+        self.openlayer: Signal = Signal(self)
+
+        #: emitted when a window is opened	WINDOWADDRESS,WORKSPACENAME,WINDOWCLASS,WINDOWTITLE
+        self.openwindow: Signal = Signal(self)
+
+        #: emitted when a window is pinned or unpinned	WINDOWADDRESS,PINSTATE
+        self.pin: Signal = Signal(self)
+
+        #: emitted when a workspace is renamed	WORKSPACEID,NEWNAME
+        self.renameworkspace: Signal = Signal(self)
+
+        #: state,handle where the state is a toggle status and the handle is one or more window addresses separated by a comma e.g. 0,0x64cea2525760,0x64cea2522380 where 0 means that a group has been destroyed and the rest informs which windows were part of it	0/1,WINDOWADDRESS(ES)
+        self.returns: Signal = Signal(self)
+
+        #: emitted when a screencopy state of a client changes. Keep in mind there might be multiple separate clients. State is 0/1, owner is 0 - monitor share, 1 - window share	STATE,OWNER
+        self.screencast: Signal = Signal(self)
+
+        #: emitted when a keybind submap changes. Empty means default.	SUBMAPNAME
+        self.submap: Signal = Signal(self)
+
+        #: emitted when togglegroup command is used.
+        self.togglegroup: Signal = Signal(self)
+
+        #: emitted when a window requests an urgent state	WINDOWADDRESS
+        self.urgent: Signal = Signal(self)
+
+        #: emitted when a window title changes.	WINDOWADDRESS
+        self.windowtitle: Signal = Signal(self)
+
+        #: emitted on workspace change. Is emitted ONLY when a user requests a workspace change, and is not emitted on mouse movements (see activemon)	WORKSPACENAME
+        self.workspace: Signal = Signal(self)
+
+        #: emitted on workspace change. Is emitted ONLY when a user requests a workspace change, and is not emitted on mouse movements (see activemon)	WORKSPACEID,WORKSPACENAME
+        self.workspacev2 : Signal = Signal(self)
 
 
 class Instance:
@@ -41,25 +301,30 @@ class Instance:
         #: The Hyprland command socket for this instance.
         self.command_socket: CommandSocket = CommandSocket(signature)
 
+        #: .. admonition:: |:no_entry_sign:| **Deprecated since v0.1.7**
+        #:
+        #:    This signal has been renamed to :attr:`~signal_workspace` and will be removed in future versions.
+        #:
         #: Signal emitted when a new workspace gets created.
         #: Sends ``created_workspace_id``, the :attr:`~hyprpy.components.workspaces.Workspace.id` of the created workspace, as signal data.
-        self.signal_workspace_created: Signal = Signal(self)
+        self.signal_workspace_created: Signal = DeprecatedSignal(
+            self, "The 'signal_workspace_created' Signal is deprecated. You should use 'Instance.signals.workspace' instead."
+        )
         #: Signal emitted when an existing workspace gets destroyed.
         #: Sends ``destroyed_workspace_id``, the :attr:`~hyprpy.components.workspaces.Workspace.id` of the destroyed workspace, as signal data
-        self.signal_workspace_destroyed: Signal = Signal(self)
+        self.signal_workspace_destroyed: Signal = DeprecatedSignal(self)
         #: Signal emitted when the focus changes to another workspace.
         #: Sends ``active_workspace_id``, the :attr:`~hyprpy.components.workspaces.Workspace.id` of the now active workspace, as signal data.
-        self.signal_active_workspace_changed: Signal = Signal(self)
-
+        self.signal_active_workspace_changed: Signal = DeprecatedSignal(self)
         #: Signal emitted when a new window gets created.
         #: Sends ``created_window_address``, the :attr:`~hyprpy.components.windows.Window.address` of the newly created window, as signal data.
-        self.signal_window_created: Signal = Signal(self)
+        self.signal_window_created: Signal = DeprecatedSignal(self)
         #: Signal emitted when an existing window gets destroyed.
         #: Sends ``destroyed_window_address``, the :attr:`~hyprpy.components.windows.Window.address` of the destroyed window, as signal data.
-        self.signal_window_destroyed: Signal = Signal(self)
+        self.signal_window_destroyed: Signal = DeprecatedSignal(self)
         #: Signal emitted when the focus changes to another window.
         #: Sends ``active_window_address``, the :attr:`~hyprpy.components.windows.Window.address` of the now active window, as signal data.
-        self.signal_active_window_changed: Signal = Signal(self)
+        self.signal_active_window_changed: Signal = DeprecatedSignal(self)
 
 
     def __repr__(self):
